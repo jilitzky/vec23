db1d6812dfd159904c50c67382c4d4c64813c8e7
Jose Ilitzky <josilit@gmail.com>
2026-01-16 09:50:35 -0800

    Implemented basic operators (#2)
    * Implemented basic operators
    
    * Remove unused test class

--- include/Vector2.h
+++ include/Vector2.h
@@ -8,5 +8,37 @@ namespace Vec23
     {
         float x;
         float y;
+
+        // TODO: Why was = default not working?
+        Vector2() : x(0.f), y(0.f) {}
+
+        Vector2(float x, float y) : x(x), y(y) {}
+
+        Vector2 operator+(const Vector2& other)
+        {
+            return { x + other.x, y + other.y };
+        }
+
+        Vector2 operator-(const Vector2& other)
+        {
+            return { x - other.x, y - other.y };
+        }
+
+        // TODO: Figure out when it's useful to use component-wise multiplication
+        Vector2 operator*(const Vector2& other)
+        {
+            return { x * other.x, y * other.y };
+        }
+
+        Vector2 operator*(float scalar)
+        {
+            return { x * scalar, y * scalar };
+        }
+
+        // TODO: Can this be optimized?
+        Vector2 operator/(float scalar)
+        {
+            return { x / scalar, y / scalar };
+        }
     };
 }
--- test/Vector2Test.cpp
+++ test/Vector2Test.cpp
@@ -3,10 +3,101 @@
 #include <gtest/gtest.h>
 #include "Vector2.h"
 
-class Vector2Test : public ::testing::Test
-{
-};
+using namespace Vec23;
 
-TEST_F(Vector2Test, Add)
-{
+// -----------------------------------------------------------------------------
+// 1. Constructor Tests
+// -----------------------------------------------------------------------------
+
+TEST(Vector2Test, DefaultConstructor) {
+    Vector2 v;
+    EXPECT_FLOAT_EQ(v.x, 0.0f);
+    EXPECT_FLOAT_EQ(v.y, 0.0f);
 }
+
+//TEST(Vector2Test, ScalarConstructor) {
+//    Vector2 v(5.0f);
+//    EXPECT_FLOAT_EQ(v.x, 5.0f);
+//    EXPECT_FLOAT_EQ(v.y, 5.0f);
+//}
+
+TEST(Vector2Test, ComponentConstructor) {
+    Vector2 v(2.0f, 3.0f);
+    EXPECT_FLOAT_EQ(v.x, 2.0f);
+    EXPECT_FLOAT_EQ(v.y, 3.0f);
+}
+
+// -----------------------------------------------------------------------------
+// 2. Operator Tests
+// -----------------------------------------------------------------------------
+
+TEST(Vector2Test, Addition) {
+    Vector2 v1(1.0f, 2.0f);
+    Vector2 v2(3.0f, 4.0f);
+    Vector2 result = v1 + v2;
+
+    EXPECT_FLOAT_EQ(result.x, 4.0f);
+    EXPECT_FLOAT_EQ(result.y, 6.0f);
+}
+
+TEST(Vector2Test, Subtraction) {
+    Vector2 v1(5.0f, 6.0f);
+    Vector2 v2(2.0f, 3.0f);
+    Vector2 result = v1 - v2;
+
+    EXPECT_FLOAT_EQ(result.x, 3.0f);
+    EXPECT_FLOAT_EQ(result.y, 3.0f);
+}
+
+TEST(Vector2Test, MultiplicationScalar) {
+    Vector2 v(2.0f, 4.0f);
+    Vector2 result = v * 2.0f;
+
+    EXPECT_FLOAT_EQ(result.x, 4.0f);
+    EXPECT_FLOAT_EQ(result.y, 8.0f);
+}
+
+//TEST(Vector2Test, MultiplicationGlobalScalar) {
+//    Vector2 v(2.0f, 4.0f);
+//    // Tests the "float * vector" global operator
+//    Vector2 result = 2.0f * v;
+//
+//    EXPECT_FLOAT_EQ(result.x, 4.0f);
+//    EXPECT_FLOAT_EQ(result.y, 8.0f);
+//}
+
+TEST(Vector2Test, ComponentMultiplication) {
+    Vector2 v1(2.0f, 3.0f);
+    Vector2 v2(4.0f, 5.0f);
+    Vector2 result = v1 * v2;
+
+    EXPECT_FLOAT_EQ(result.x, 8.0f);
+    EXPECT_FLOAT_EQ(result.y, 15.0f);
+}
+
+//TEST(Vector2Test, Negation) {
+//    Vector2 v(1.0f, -2.0f);
+//    Vector2 result = -v;
+//
+//    EXPECT_FLOAT_EQ(result.x, -1.0f);
+//    EXPECT_FLOAT_EQ(result.y, 2.0f);
+//}
+//
+//TEST(Vector2Test, CompoundAddition) {
+//    Vector2 v1(1.0f, 2.0f);
+//    Vector2 v2(3.0f, 4.0f);
+//    v1 += v2;
+//
+//    EXPECT_FLOAT_EQ(v1.x, 4.0f);
+//    EXPECT_FLOAT_EQ(v1.y, 6.0f);
+//}
+//
+//TEST(Vector2Test, SubscriptOperator) {
+//    Vector2 v(5.0f, 10.0f);
+//    EXPECT_FLOAT_EQ(v[0], 5.0f);
+//    EXPECT_FLOAT_EQ(v[1], 10.0f);
+//
+//    // Test write via subscript
+//    v[0] = 7.0f;
+//    EXPECT_FLOAT_EQ(v.x, 7.0f);
+//}
