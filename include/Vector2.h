93f772b4bbb9cbbe47da364ee8961ddcf1606807
Jose Ilitzky <josilit@gmail.com>
2026-01-16 21:48:36 -0800

    Finished implementing operators

--- include/Vector2.h
+++ include/Vector2.h
@@ -12,33 +12,116 @@ namespace Vec23
         // TODO: Why was = default not working?
         Vector2() : x(0.f), y(0.f) {}
 
+        Vector2(float scalar) : x(scalar), y(scalar) {}
+
         Vector2(float x, float y) : x(x), y(y) {}
 
-        Vector2 operator+(const Vector2& other)
+        Vector2 operator+(const Vector2& other) const
         {
             return { x + other.x, y + other.y };
         }
 
-        Vector2 operator-(const Vector2& other)
+        Vector2 operator-(const Vector2& other) const
         {
             return { x - other.x, y - other.y };
         }
 
-        // TODO: Figure out when it's useful to use component-wise multiplication
-        Vector2 operator*(const Vector2& other)
+        // TODO: Figure out why it's useful to do component-wise multiplication
+        Vector2 operator*(const Vector2& other) const
         {
             return { x * other.x, y * other.y };
         }
 
-        Vector2 operator*(float scalar)
+        Vector2 operator*(float scalar) const
         {
             return { x * scalar, y * scalar };
         }
 
-        // TODO: Can this be optimized?
-        Vector2 operator/(float scalar)
+        Vector2 operator/(float scalar) const
         {
-            return { x / scalar, y / scalar };
+            float multiplier = 1.f / scalar;
+            return (*this * multiplier);
         }
+
+        Vector2 operator-() const
+        {
+            return { -x, -y };
+        }
+
+        Vector2& operator+=(const Vector2& other)
+        {
+            x += other.x;
+            y += other.y;
+            return *this;
+        }
+
+        // TODO: Should subtraction be implemented based on addition?
+        Vector2& operator-=(const Vector2& other)
+        {
+            x -= other.x;
+            y -= other.y;
+            return *this;
+        }
+
+        // TODO: Why are there no unit tests for this?
+        Vector2& operator*=(float scalar)
+        {
+            x *= scalar;
+            y *= scalar;
+            return *this;
+        }
+
+        Vector2& operator/=(float scalar)
+        {
+            *this *= 1.f / scalar;
+            return *this;
+        }
+
+        bool operator==(const Vector2& other) const
+        {
+            return (x == other.x) && (y == other.y);
+        }
+
+        bool operator!=(const Vector2& other) const
+        {
+            return !(*this == other);
+        }
+
+        float& operator[](int index)
+        {
+            switch (index)
+            {
+            case 0:
+                return x;
+            case 1:
+                return y;
+            default:
+                // TODO: What do we do here?
+                static float zero = 0.f;
+                return zero;
+            }
+        }
+
+        // TODO: Can we reuse the non-const accessor?
+        const float& operator[](int index) const
+        {
+            switch (index)
+            {
+            case 0:
+                return x;
+            case 1:
+                return y;
+            default:
+                // TODO: What do we do here?
+                static float zero = 0.f;
+                return zero;
+            }
+        }
+
+        // TODO: Why is this not working?
+        //Vector2 operator*(float scalar, const Vector2& vector)
+        //{
+        //    return vector * scalar;
+        //}
     };
 }
--- test/Vector2Test.cpp
+++ test/Vector2Test.cpp
@@ -15,11 +15,11 @@ TEST(Vector2Test, DefaultConstructor) {
     EXPECT_FLOAT_EQ(v.y, 0.0f);
 }
 
-//TEST(Vector2Test, ScalarConstructor) {
-//    Vector2 v(5.0f);
-//    EXPECT_FLOAT_EQ(v.x, 5.0f);
-//    EXPECT_FLOAT_EQ(v.y, 5.0f);
-//}
+TEST(Vector2Test, ScalarConstructor) {
+    Vector2 v(5.0f);
+    EXPECT_FLOAT_EQ(v.x, 5.0f);
+    EXPECT_FLOAT_EQ(v.y, 5.0f);
+}
 
 TEST(Vector2Test, ComponentConstructor) {
     Vector2 v(2.0f, 3.0f);
@@ -75,29 +75,29 @@ TEST(Vector2Test, ComponentMultiplication) {
     EXPECT_FLOAT_EQ(result.y, 15.0f);
 }
 
-//TEST(Vector2Test, Negation) {
-//    Vector2 v(1.0f, -2.0f);
-//    Vector2 result = -v;
-//
-//    EXPECT_FLOAT_EQ(result.x, -1.0f);
-//    EXPECT_FLOAT_EQ(result.y, 2.0f);
-//}
-//
-//TEST(Vector2Test, CompoundAddition) {
-//    Vector2 v1(1.0f, 2.0f);
-//    Vector2 v2(3.0f, 4.0f);
-//    v1 += v2;
-//
-//    EXPECT_FLOAT_EQ(v1.x, 4.0f);
-//    EXPECT_FLOAT_EQ(v1.y, 6.0f);
-//}
-//
-//TEST(Vector2Test, SubscriptOperator) {
-//    Vector2 v(5.0f, 10.0f);
-//    EXPECT_FLOAT_EQ(v[0], 5.0f);
-//    EXPECT_FLOAT_EQ(v[1], 10.0f);
-//
-//    // Test write via subscript
-//    v[0] = 7.0f;
-//    EXPECT_FLOAT_EQ(v.x, 7.0f);
-//}
+TEST(Vector2Test, Negation) {
+    Vector2 v(1.0f, -2.0f);
+    Vector2 result = -v;
+
+    EXPECT_FLOAT_EQ(result.x, -1.0f);
+    EXPECT_FLOAT_EQ(result.y, 2.0f);
+}
+
+TEST(Vector2Test, CompoundAddition) {
+    Vector2 v1(1.0f, 2.0f);
+    Vector2 v2(3.0f, 4.0f);
+    v1 += v2;
+
+    EXPECT_FLOAT_EQ(v1.x, 4.0f);
+    EXPECT_FLOAT_EQ(v1.y, 6.0f);
+}
+
+TEST(Vector2Test, SubscriptOperator) {
+    Vector2 v(5.0f, 10.0f);
+    EXPECT_FLOAT_EQ(v[0], 5.0f);
+    EXPECT_FLOAT_EQ(v[1], 10.0f);
+
+    // Test write via subscript
+    v[0] = 7.0f;
+    EXPECT_FLOAT_EQ(v.x, 7.0f);
+}
